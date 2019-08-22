221700c1c5d80f82094f766e69b0771f5068c8a9
krzysztof <235060@student.pwr.edu.pl>
2019-08-22 16:49:38 +0200

    auth start

---
+++ DogFinder/Scenes/Authentication/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  DogFinder
+//
+//  Created by Krzysztof Kapała on 22/08/2019.
+//  Copyright © 2019 Krzysztof Kapała. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private let viewModel: LoginViewModel = LoginViewModel()
+    
+    
+    
+}
---
+++ DogFinder/Scenes/Authentication/Login/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  DogFinder
+//
+//  Created by Krzysztof Kapała on 22/08/2019.
+//  Copyright © 2019 Krzysztof Kapała. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewModel {
+    
+    
+}
