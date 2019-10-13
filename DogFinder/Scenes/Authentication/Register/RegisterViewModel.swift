8a83594c6db5dc50a10db8536e71c362e1ed8b9d
krzysztof <235060@student.pwr.edu.pl>
2019-10-13 08:38:47 +0200

    register vm

---
+++ DogFinder/Scenes/Authentication/Register/RegisterViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewModel.swift
+//  DogFinder
+//
+//  Created by Krzysztof Kapała on 12/10/2019.
+//  Copyright © 2019 Krzysztof Kapała. All rights reserved.
+//
+
+import Foundation
+
+class RegisterViewModel {
+    
+    func register(username:String?, password: String?, email: String?, completion: @escaping (()->Void)) {
+        
+        guard let username = username else { return }
+        guard let password = password else { return }
+        guard let email = email else { return }
+        
+        DogFinderApi.sharedInstance.register(username: username, password: password, email: email, completionHandler: completion) { (error) in
+            print(error)
+        }
+    }
+}
